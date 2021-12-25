ba08bdf86c3bedeabd1446f2e83563a988959ce2
BEU <evbolohov@yandex.ru>
2021-12-25 14:42:13 +0300

    init

--- Основы ООП/Практические задания/task1_Glass__init__/main.py
+++ Основы ООП/Практические задания/task1_Glass__init__/main.py
@@ -3,10 +3,35 @@ from typing import Union
 
 class Glass:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
-        #  TODO инициализировать объект "Стакан"
+        self.capacity_volume = self.__check_capacity_volume(capacity_volume)
+        self.occupied_volume = self.__check_occupied_volume(occupied_volume)
+        # self.__check_overflow(self.capacity_volume, self.occupied_volume)
+        self.__check_overflow()
 
+    @staticmethod
+    def __check_capacity_volume(value) -> Union[int, float]:
+        """
+        :param value:
+        :return:
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value <= 0:
+            raise ValueError
+        return value
 
-if __name__ == "__main__":
-    ...  # TODO инициализировать два объекта типа Glass
+    @staticmethod
+    def __check_occupied_volume(value) -> Union[int, float]:
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        return value
 
-    # TODO попробовать инициализировать не корректные объекты
+    # @staticmethod
+    # def __check_overflow(capacity, occupied):
+    #     if capacity < occupied:
+    #         raise OverflowError('Стакан не резиновый')
+    def __check_overflow(self):
+        if self.capacity_volume < self.occupied_volume:
+            raise OverflowError('Стакан не резиновый')
