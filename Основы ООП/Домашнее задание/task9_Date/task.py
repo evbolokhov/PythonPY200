385a566ef362a22d3203f4f6c87a94fb8188fb57
BEU <evbolohov@yandex.ru>
2022-01-16 01:08:36 +0300

    task dz1

--- Основы ООП/Домашнее задание/task9_Date/task.py
+++ Основы ООП/Домашнее задание/task9_Date/task.py
@@ -1,1 +1,31 @@
+class Date:
+    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        self.capacity_volume = self.__check_capacity_volume(capacity_volume)
+        self.occupied_volume = self.__check_occupied_volume(occupied_volume)
+        # self.__check_overflow(self.capacity_volume, self.occupied_volume)
+        self.__check_overflow()
+
+    @staticmethod
+    def __check_capacity_volume(value) -> Union[int, float]:
+        """
+        :param value:
+        :return:
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value <= 0:
+            raise ValueError
+        return value
+
+    @staticmethod
+    def __check_occupied_volume(value) -> Union[int, float]:
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        return value
+
+
+
+
 # TODO class Date
