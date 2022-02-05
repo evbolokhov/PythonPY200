2938417ed33523b7ab373464a7f11d9c5b00eac8
BEU <evbolohov@yandex.ru>
2022-02-05 13:44:43 +0300

    task zachet

--- Зачетное задание/Зачетное задание/DoubleLinkedList/node.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/node.py
@@ -1,1 +1,58 @@
-...
+from typing import Any, Optional
+import weakref
+
+
+class Node:
+    """ Класс, который описывает узел связного списка. """
+
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        """
+        Создаем новый узел для односвязного списка
+        :param value: Любое значение, которое помещено в узел
+        :param next_: следующий узел, если он есть
+        """
+        self.value = value
+        self.next = next_  # вызовется setter
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def is_valid(self, node: Any) -> None:
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next_: Optional["Node"]):
+        self.is_valid(next_)
+        self._next = next_
+
+    def __del__(self):
+        print("Вызван метод __del__ узла")
+
+
+class DoubleLinkedNode(Node):
+    def __init__(self, value: Any, prev: Optional["Node"] = None, next_: Optional["Node"] = None):
+        super().__init__(value, next_)
+        self.prev = prev
+
+    @property
+    def prev(self):
+        return None if self._prev is None else self._prev()
+
+    @prev.setter
+    def prev(self, prev: Optional["Node"]):
+        self.is_valid(prev)
+        self._prev = None if prev is None else weakref.ref(prev)   # TODO сделать слабую ссылку
+
+    def __repr__(self) -> str:
+        next_prev = None if self.prev is None else f"DoubleLinkedNode({self.prev})"
+        next_repr = None if self.next is None else f"DoubleLinkedNode({self.next})"  # todo make all
+
+        return f"DoubleLinkedNode({self.value}, {next_prev}, {next_repr})"
--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -1,10 +1,120 @@
+import sys
+from typing import Iterator, Any, Iterable, Optional
+from node import Node
+from node import DoubleLinkedNode
+
+
 class LinkedList:
-    ...
+    node_class = Node
+
+    def __init__(self, data: Iterable = None):
+        """Конструктор связного списка"""
+        self.len = 0
+        self.head: Optional[Node] = None
+        self.tail = self.head
+
+        if data is not None:
+            for value in data:
+                self.append(value)
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = self.node_class(value)
+
+        if self.head is None:
+            self.head = self.tail = append_node
+        else:
+            self.linked_nodes(self.tail, append_node)
+            self.tail = append_node
+
+        self.len += 1
+
+    def step_by_step_on_nodes(self, index: int) -> Node:
+        """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
+        if not isinstance(index, int):
+            raise TypeError()
+
+        if not 0 <= index < self.len:  # для for
+            raise IndexError()
+
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        return current_node
+
+    @staticmethod
+    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
+        """
+        Функция, которая связывает между собой два узла.
+
+        :param left_node: Левый или предыдущий узел
+        :param right_node: Правый или следующий узел
+        """
+        left_node.next = right_node
+
+    def __getitem__(self, index: int) -> Any:
+        """ Метод возвращает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        return node.value
+
+    def __setitem__(self, index: int, value: Any) -> None:
+        """ Метод устанавливает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        node.value = value
+
+    def to_list(self) -> list:
+        return [linked_list_value for linked_list_value in self]
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.to_list()})"
+
+    def __str__(self) -> str:
+        return f"{self.to_list()}"
+
+    def nodes_iterator(self) -> Iterator[Node]:
+        current_node = self.head
+        for _ in range(self.len):
+            yield current_node
+            current_node = current_node.next
+
+    def print_ref_count(self):
+        current_node = self.head
+        for i in range(self.len):
+            print(f"Количество ссылок на {i} узел:", sys.getrefcount(current_node))
+            current_node = current_node.next
+
+    def clear(self):
+        self.len = 0
+        self.head = None
+        self.tail = None
 
 
 class DoubleLinkedList(LinkedList):
-    ...
+
+    @staticmethod
+    def linked_nodes(left_node: DoubleLinkedNode, right_node: Optional[DoubleLinkedNode] = None) -> None:
+        left_node.next = right_node
+        right_node.prev = left_node
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = DoubleLinkedNode(value)
+
+        if self.head is None:
+            self.head = self.tail = append_node
+        else:
+            self.linked_nodes(self.tail, append_node)
+            self.tail = append_node
+
+        self.len += 1
+
+
+def test_append_double_linked_nodes():
+    dll = DoubleLinkedList([1, 2, 3])
+    print(type(dll.head))
 
 
 if __name__ == "__main__":
-    ...
+    test_append_double_linked_nodes()
+
